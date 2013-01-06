3da06bfdf10a6d3be6ffbd724f1cc65677185090
John Kelly <jkelly0@me.com>
2013-01-05 16:22:26 -0800

    Update rails to 3.2.10
    Update gem file specifically rails to latest versions

--- spec/support/fixtures_builder.rb
+++ spec/support/fixtures_builder.rb
@@ -2,8 +2,7 @@ require 'build_models'
 
 FixtureBuilder.configure do |config|
   config.files_to_check += Dir[*%w[spec/support/fixture_builder.rb lib/build_models.rb]]
-  config.record_name_fields << "email" << "question"
-
+  config.record_name_fields << "email"
   config.factory do
     instance_eval &BuildModels.builder
   end
