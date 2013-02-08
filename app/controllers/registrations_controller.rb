f9f450937d4206deebd2cc0ec43fb4cefe622bee
John Kelly <jkelly0@me.com>
2013-02-08 00:09:30 -0800

    Refactor registrations and track create account

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -12,23 +12,40 @@ class RegistrationsController < Devise::RegistrationsController
 
     if resource.save
       if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_navigational_format?
-        sign_in(resource_name, resource)
-        respond_with resource, :location => after_sign_up_path_for(resource)
+        sign_up_success(resource)
       else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-        expire_session_data_after_sign_in!
-        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+        sign_up_success_but_inactive(resource)
       end
     else
-      clean_up_passwords resource
-      @plan = params[:plan]
-      flash.now[:alert] = resource.errors.full_messages.first
-      respond_with resource
+      sign_up_failure(resource)
     end
   end
 
+  protected
+
   def after_sign_up_path_for(resource)
     subscribers_path
+  end
+
+  private
+
+  def sign_up_success_but_inactive(resource)
+    set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+    expire_session_data_after_sign_in!
+    respond_with resource, location: after_inactive_sign_up_path_for(resource)
+  end
+
+  def sign_up_success(resource)
+    set_flash_message :notice, :signed_up if is_navigational_format?
+    flash[:analytics] = "/vp/create_account"
+    sign_in(resource_name, resource)
+    respond_with resource, location: after_sign_up_path_for(resource)
+  end
+
+  def sign_up_failure(resource)
+    clean_up_passwords resource
+    @plan = params[:plan]
+    flash.now[:alert] = resource.errors.full_messages.first
+    respond_with resource
   end
 end
