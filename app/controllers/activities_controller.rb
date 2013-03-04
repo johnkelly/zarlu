08be1a86839a861603aa78c99b2b22fd92eb7648
John Kelly <jkelly0@me.com>
2013-03-03 16:35:03 -0800

    Fix n + 1 issue on activity feed page

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,15 +4,17 @@ class ActivitiesController < ApplicationController
 
   def index
     @level_of_activity = params[:activity_type].presence || "user"
+    subscriber = current_user.subscriber
+    @managers = subscriber.managers(subscriber.users)
     case @level_of_activity
     when "user"
-      @activities = current_user.activities.order("created_at desc").limit(50)
+      @activities = current_user.activities.order("created_at desc").limit(50).includes(:user)
     when "manager"
       employees = current_user.employees.collect(&:id)
-      @activities = Activity.order("created_at desc").where(user_id: employees).limit(50)
+      @activities = Activity.order("created_at desc").where(user_id: employees).limit(50).includes(:user)
     when "company"
       company = current_user.subscriber.users.collect(&:id)
-      @activities = Activity.order("created_at desc").where(user_id: company).limit(50)
+      @activities = Activity.order("created_at desc").where(user_id: company).limit(50).includes(:user)
     end
   end
 end
