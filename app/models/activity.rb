a4f43b028970ddbe62a305b7184dbf081efe2acb
John Kelly <jkelly0@me.com>
2013-03-04 20:15:51 -0800

    Render activities as collection

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -5,4 +5,8 @@ class Activity < ActiveRecord::Base
   validates_presence_of :user_id, :trackable_id
 
   attr_accessible :action, :trackable
+
+  def to_partial_path
+    "activities/#{trackable_type.underscore}s/#{action}"
+  end
 end
