4d42129ccd34b266c1decae608256533b98e95f9
John Kelly <jkelly0@me.com>
2013-05-21 20:26:31 -0700

    Move static assets to amazon cloudfront

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Zarlu::Application.configure do
   config.cache_store = :dalli_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "https://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
+  config.action_controller.asset_host = "https://d1wbnlnsk3c03o.cloudfront.net"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
