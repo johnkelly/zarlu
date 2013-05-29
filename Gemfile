594eb35a515173be9a6eba8779d6fc64d0b72aa6
John Kelly <jkelly0@me.com>
2013-05-29 00:38:35 -0700

    Add rack cache

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'bootstrap-sass', '2.3.1.2'
 gem 'd3_rails', '~> 3.1.9'
 gem 'asset_sync', '~> 0.5.4'
 gem 'heroku-api', '~> 0.3.11'
+gem 'rack-cache', '~> 1.2'
 
 group :development, :test do
   gem 'rspec-rails', git: 'git://github.com/rspec/rspec-rails.git'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Zarlu::Application.configure do
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
-  # config.action_dispatch.rack_cache = true
+  config.action_dispatch.rack_cache = true
 
   # Set caching of assets on Heroku
   config.serve_static_assets = true
